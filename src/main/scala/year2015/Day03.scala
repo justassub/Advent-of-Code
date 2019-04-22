fe2cf0446a867a7eef5d15954ed49ab23293f86b
Justas Subatavicius <justassub@gmail.com>
2019-04-22 21:22:17 +0300

    Day 3 part 2
    Implemented part 2
    
    Co-Authored-By: Justas Subatavicius <jussbt@telesoftas.com>

--- src/main/scala/year2015/Day03.scala
+++ src/main/scala/year2015/Day03.scala
@@ -4,6 +4,7 @@ case class History(x: Int, y: Int)
 
 object Day03 extends Main2015[String, Int, Int] {
   println(part1(seqString))
+  println(part2(seqString))
 
   override def part1(seq: Seq[String]) = {
     seq
@@ -15,7 +16,12 @@ object Day03 extends Main2015[String, Int, Int] {
   }
 
 
-  override def part2(seq: Seq[String]) = ???
+  override def part2(seq: Seq[String]) = {
+    val (santaRoute, robotRoute) = seq.head.toCharArray.zipWithIndex.partition(_._2 % 2 == 0)
+    val santaHouses = santaRoute.map(_._1).scanLeft(History(0, 0))(Day03Util.changeHistory).distinct.toList
+    val robotHouses = robotRoute.map(_._1).scanLeft(History(0, 0))(Day03Util.changeHistory).distinct.toList
+    (santaHouses ::: robotHouses).distinct.size
+  }
 }
 
 object Day03Util {
--- src/test/scala/year2015/Day3Test.scala
+++ src/test/scala/year2015/Day3Test.scala
@@ -8,5 +8,9 @@ class Day3Test extends FlatSpec with Matchers {
     Day03.part1(Seq("^>v<")) shouldEqual 4
     Day03.part1(Seq("^v^v^v^v^v")) shouldEqual 2
   }
-
+  "Day 3 part 2" should "be equals to given examples" in {
+    Day03.part2(Seq("^v")) shouldEqual 3
+    Day03.part2(Seq("^>v<")) shouldEqual 3
+    Day03.part2(Seq("^v^v^v^v^v")) shouldEqual 11
+  }
 }
