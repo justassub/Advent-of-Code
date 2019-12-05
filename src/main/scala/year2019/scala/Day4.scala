3d2d012596a685cdae930fe75b470d5a98dc907e
Justas Subatavicius <justassub@gmail.com>
2019-12-05 21:32:10 +0200

    add regex for day 4

--- src/main/scala/year2019/scala/Day4.scala
+++ src/main/scala/year2019/scala/Day4.scala
@@ -1,6 +1,11 @@
 package year2019.scala
 
+
 object Day4 extends Main2019[Int, Int, Int] {
+  val part1Regex = "^(?=[0-9]{6}$)(?=.*([0-9])\\1)(?:0|1(?!0)|2(?![01])|3(?![0-2])|4(?![0-3])|5(?![0-4])|6(?![0-5])|7(?![0-6])|8(?![0-7])|9(?![0-8]))+$"
+  val part2Regex = "^(?=[0-9]{6}$)(?=(?:.*([0-9])(?!\\1))?([0-9])\\2(?!\\2))(?:0|1(?!0)|2(?![01])|3(?![0-2])|4(?![0-3])|5(?![0-4])|6(?![0-5])|7(?![0-6])|8(?![0-7])|9(?![0-8]))+$"
+  val part3Regex = "(.)(?<!\1\1)\1(?!\1)"
+
   //TODO add tests
   override def part1(seq: Seq[Int]) = {
     seq.count(isPassword)
@@ -13,6 +18,7 @@ object Day4 extends Main2019[Int, Int, Int] {
   def containsAdjanced(password: String) = password.sliding(2).count(p => p(0) == p(1)) > 0
 
   def notIncreases(password: String) = password.toCharArray.map(_.asDigit).sorted.mkString == password
+
   //TODO add tests
   override def part2(seq: Seq[Int]) = {
     //Finds numbers that are repeated only one. //TODO refactor with function programming
@@ -31,7 +37,13 @@ object Day4 extends Main2019[Int, Int, Int] {
       countAdj.contains(1)
     }
     seq.count(p => adjTwice(p) && isPassword(p))
+
   }
+
+  def isPassword1Regex(int: Int) = int.toString.matches(part1Regex)
+
+  def isPassword2Regex(int: Int) = int.toString.matches(part2Regex)
+
 
   println(part1(367479 to 893698))
   println(part2(367479 to 893698))
