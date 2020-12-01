2d68bb7f19e45cd3b7d90e1c7253420972cb08e2
JustasSub <justassub@gmail.com>
2020-12-01 07:27:36 +0200

    Day 1 implementation

--- src/main/scala/year2020/java/Main2020.java
+++ src/main/scala/year2020/java/Main2020.java
@@ -5,6 +5,6 @@ import util.MainJava;
 public abstract class Main2020<I1, I2, O1, O2> extends MainJava<I1, I2, O1, O2> {
     @Override
     protected final String getPackage() {
-        return "2019";
+        return "2020";
     }
 }
