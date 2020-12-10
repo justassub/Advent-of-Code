edad2e65647aa81161885c3ac78f31837c120e4b
JustasSub <justassub@gmail.com>
2020-12-10 08:12:32 +0200

    Day 10 implemented

---
+++ src/main/java/lt/justassub/adventofcode/year2020/day10/Jolt.java
@@ -0,0 +1,22 @@
+package lt.justassub.adventofcode.year2020.day10;
+
+public class Jolt {
+    private int value;
+    private int difference;
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public int getDifference() {
+        return difference;
+    }
+
+    public void setDifference(int difference) {
+        this.difference = difference;
+    }
+}
